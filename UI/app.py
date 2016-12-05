c91b67ca42a4f00282fe0a15f116a1ea3218c2a3
Muhammad Tarek <muhammadtarek96@gmail.com>
2016-12-06 00:18:16 +0200

    Adding UGLY Text field and round button

--- UI/app.py
+++ UI/app.py
@@ -1,24 +1,55 @@
 import os
 import sys
-from PyQt4 import QtGui, QtCore
-from PyQt4 import Qt
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
 
-class mainScreen(QtGui.QWidget):
+class mainScreen(QWidget):
+    #
+    #Calling the super to initialize the window
+    #
     def __init__(self, parent = None):
         super(mainScreen, self).__init__()
-        self.initUI()
+        self.initMainWindow()
 
-    def initUI(self):
+    def initMainWindow(self):
         #Window sepc
-        self.setGeometry(0,0,1000,600)
+        self.resize(1000,600)
+        self.setFixedSize(self.size())
         self.setWindowTitle("Auto Compelete Code")
         #Centering the screen
-        self.move(QtGui.QApplication.desktop().availableGeometry().center() - self.frameGeometry().center())
+        #self.move(QtGui.QApplication.desktop().availableGeometry().center() - self.frameGeometry().center())
 
+        #Connect CSS File
+        self.styleSheet = ''
+        cssFile = open('style.css', 'r')
+        self.styleSheet = cssFile.read()
+        cssFile.close()
+        self.setStyleSheet(self.styleSheet)
+
+        #initialize main widget
+        mainLayout = QGridLayout()
+
+        #Creating main widget
+        self.setLayout(mainLayout)
+        self.initTextEditor(mainLayout)
         self.show()
 
+    def initTextEditor(self, mainLayout):
+        codeEditor = QTextEdit("TEST")
+        mainLayout.addWidget(codeEditor, 0, 0)
+        codeEditor.setFontFamily("Monospace")
+        #font = QFont("OperatorMono-Light", 10)
+        #codeEditor.setFont(font)
+        #Resize the textfield to fit the screen
+        codeEditor.resize(codeEditor.document().size().width(), codeEditor.document().size().height() + 10);
+
+        importFileButton = QPushButton("+", self)
+        importFileButton.move(self.width() * 0.9, self.height() * 0.84)
+
+
 def main():
-    app = QtGui.QApplication(sys.argv);
+    app = QApplication(sys.argv)
+    #Creating object from mainScreen
     main = mainScreen()
     sys.exit((app.exec_()))
 
